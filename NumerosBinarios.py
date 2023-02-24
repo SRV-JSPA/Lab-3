9a6786b7960751c48f4e1be2abe4f6bb8a5db038
Santiago Pereira <97372983+SRV-JSPA@users.noreply.github.com>
2023-02-23 21:13:29 -0600

    Update NumerosBinarios.py

--- NumerosBinarios.py
+++ NumerosBinarios.py
@@ -35,3 +35,26 @@ def C2():
     lista = list(bin(decimal)[2:])
 
     print("En C2",lista)
+   
+   def hexD():
+
+    
+    s=input("Ingrese el numero Hexadecimal para converirlo a decimal \n")
+
+    DC=int(s,16)
+
+    print("El numero hexadecimal",s,"en decimal",DC)
+
+def Dhex():
+
+    s=input("Ingrese el numero decimal oara convertirlo a hexadecimal \n")
+
+    HX=hex(s)
+
+    print("El numero decimal",s,"en hexadecimal es",HX)
+    
+
+C1()
+C2()
+hexD()
+Dhex()
